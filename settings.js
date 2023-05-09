8384c900b99b9c5675e15267632acb9f0a15a1c9
VictorChao996 <96094victor@gmail.com>
2023-05-09 16:10:02 +0800

    bug fix

--- settings.js
+++ settings.js
@@ -1,5 +1,7 @@
-import token from "./utils/getToken.js";
+// import token from "./utils/getToken.js";
+const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwidXNlcm5hbWUiOiJTdGxpc2giLCJuaWNrX25hbWUiOiJTeWxpc2giLCJlbWFpbCI6InN5bGlzaEBzdHlsaXNoLnNob3AiLCJwaG9uZSI6IjA4NzY1NDMyMSIsInVzZXJfbGV2ZWwiOjEsImV4cCI6MTY4Mzg3NTA4Mn0.QyYuqeLb-YFcwkUrHuTY-02B1QVeGYoPHmouufzAXn8"
 export default {
     API_ENDPOINT: "http://192.168.50.104:3000/api/v1",
-    API_ACCESS_TOKEN: token,
-};
+    API_ACCESS_TOKEN: token
+}
--- utils/getToken.js
+++ utils/getToken.js
@@ -1,15 +1,14 @@
-import axios from "axios";
+import axios from "axios"
 
-let token;
-const response = await axios.post(
-    `http://member-api.appworks.local/api/v1/client/login`,
-    {
-        username: "Stlish",
-        password: "stylish123",
-    }
-);
+let token
+// let url = "http://192.168.50.102/api/v1/client/login"
+let url = "http://member-api.appworks.local/api/v1/client/login"
+const response = await axios.post(url, {
+    username: "Stlish",
+    password: "stylish123"
+})
 
-console.log(response.data);
-token = response.data.data.access_token;
+console.log(response.data)
+token = response.data.data.access_token
 
-export default token;
+export default token
