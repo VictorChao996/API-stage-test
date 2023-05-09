a9933a8d2668481dfea863a3ddf8298ab8c29fc9
VictorChao996 <96094victor@gmail.com>
2023-05-09 17:34:49 +0800

    fix: content fix

--- healthcheck.test.js
+++ healthcheck.test.js
@@ -1,13 +1,11 @@
-import axios from "axios";
-import SETTINGS from "./settings.js";
-import token from "./utils/getToken.js";
+import axios from "axios"
+import SETTINGS from "./settings.js"
+// import token from "./utils/getToken.js";
 
 describe(`healthcheck`, () => {
     it("should return 200 OK status", async () => {
-        console.log(`API endpoint: ${SETTINGS.API_ENDPOINT}/healthcheck`);
-        const response = await axios.get(
-            `${SETTINGS.API_ENDPOINT}/healthcheck`
-        );
-        expect(response.data).toHaveProperty("status", "ok");
-    });
-});
+        console.log(`API endpoint: ${SETTINGS.API_ENDPOINT}/healthcheck`)
+        const response = await axios.get(`${SETTINGS.API_ENDPOINT}/healthcheck`)
+        expect(response.data).toHaveProperty("status", "ok")
+    })
+})
--- healthcheck2.test.js
+++ healthcheck2.test.js
@@ -1,13 +1,11 @@
-import axios from "axios";
-import SETTINGS from "./settings.js";
-import token from "./utils/getToken.js";
+import axios from "axios"
 
 describe(`healthcheck`, () => {
     it("should return 200 OK status", async () => {
         // console.log(`API endpoint: ${SETTINGS.API_ENDPOINT}/healthcheck`);
         const response = await axios.get(
             `http://192.168.50.104:3000/api/v1/healthcheck`
-        );
-        expect(response.data).toHaveProperty("status", "ok");
-    });
-});
+        )
+        expect(response.data).toHaveProperty("status", "ok")
+    })
+})
